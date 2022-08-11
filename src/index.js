b9be6428b0de2fad508ef0289ed9b1ff9c8631df
Matt Vogel <mainmoniker@googlemail.com>
2022-08-10 20:30:25 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const panelConfig = {
   ]
 };
 
-await function onload({extensionAPI}) {
+async function onload({extensionAPI}) {
   // set defaults if they dont' exist
   if (!extensionAPI.settings.get('data')) {
       await extensionAPI.settings.set('data', "01");
